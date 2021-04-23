811cff0f752f10735031b32767689ceb6ddcc963
andersonwu2000 <andersonwu2000@gmail.com>
2021-04-24 05:38:27 +0800

    yea

--- dataloader.py
+++ dataloader.py
@@ -72,6 +72,7 @@ class MT5Dataset(Dataset):
                 print(f'{symbol[i]} data copy error, skip')
             else:
                 self.data += [data]
+
         self.data = torch.FloatTensor(self.data).permute(1, 0, 2)
 
     def __getitem__(self, X):
@@ -81,20 +82,20 @@ class MT5Dataset(Dataset):
         return self.data[X]
 
 class DataLoader:
+    """
+    The last incomplete batch will be smaller
+    Args:
+        dataset (MT5Dataset) : MT5Dataset
+        seq_len     (int)    : The length of the sequence
+        batch_size  (int)    : How many samples per batch to load
+        label_len   (int)    : Label's time step
+        overlap     (bool)   : Whether the sample sequence is disjoint
+        shuffle     (bool)   : Set to True to have the data reshuffled
+        batch_first (bool)   : Whether to use batch as dim=0
+        unique      (bool)   : Analysis time between unsynchronized symbols
+    """
     def __init__(self, dataset, seq_len, batch_size=1, label_len=1, 
                     overlap=True, shuffle=True, batch_first=False, unique=False):
-        """
-        The last incomplete batch will be smaller
-        Args:
-            dataset (MT5Dataset) : MT5Dataset
-            seq_len     (int)    : The length of the sequence
-            batch_size  (int)    : How many samples per batch to load
-            label_len   (int)    : Label's time step
-            overlap     (bool)   : Whether the sample sequence is disjoint
-            shuffle     (bool)   : Set to True to have the data reshuffled
-            batch_first (bool)   : Whether to use batch as dim=0
-            unique      (bool)   : Analysis time between unsynchronized symbols
-        """
         self.idx = 0
         self.dataset     = dataset
         self.seq_len     = seq_len
@@ -109,12 +110,17 @@ class DataLoader:
         else:
             step = 1
 
-        self.index = np.arange(stop=shape[0]-seq_len+1, step=step)
-        self.index = np.stack([self.index] * shape[1])
-        self.index = np.stack([self.index+i for i in range(seq_len)])
-        self.index = self.index.transpose(0, 2, 1)
-        # index shape : (seq_len, batch_num, symbols)
-        self.batch_num = self.index.shape[1]
+        self.input_idx = np.arange(0, shape[0]-seq_len-label_len+1, step=step)  # index selection
+        self.input_idx = np.stack([self.input_idx] * shape[1])                   # symbols
+        self.input_idx = np.stack([self.input_idx+i for i in range(seq_len)])    # seq_len
+        self.input_idx = self.input_idx.transpose(0, 2, 1)
+        # input_idx shape : (seq_len, shape[0]-seq_len+1, symbols)
+        self.batch_num = self.input_idx.shape[1]
+
+        self.label_idx = []
+        for i in range(label_len):
+            self.label_idx += [self.input_idx[-1] + i + i]
+        self.label_idx = torch.LongTensor(self.label_idx)
         
         if shuffle:
             self.shuffle()
@@ -122,12 +128,12 @@ class DataLoader:
 
     def shuffle(self):
         if self.unique:
-            for i in self.index[0].T:
+            for i in self.input_idx[0].T:
                 np.random.default_rng().shuffle(i)
         else:
-            np.random.default_rng().shuffle(self.index[0])
+            np.random.default_rng().shuffle(self.input_idx[0])
         for i in range(1, self.seq_len):
-            self.index[i] = self.index[i-1]+1
+            self.input_idx[i] = self.input_idx[i-1]+1
 
     def __len__(self):
         return self.batch_num
@@ -138,21 +144,25 @@ class DataLoader:
         
     def __next__(self):
         if self.idx < self.batch_num:
-            X = self.index[:, self.idx*self.batch_size:(self.idx+1)*self.batch_size]
-            # X shape : (seq_len, batch_size, symbols)
+            X = self.input_idx[:, self.idx*self.batch_size:(self.idx+1)*self.batch_size]
             inputs = self.dataset.data[X, self.Y]
             # inputs shape : (seq_len, batch_size, symbols, columns)
+            X = self.label_idx[:, self.idx*self.batch_size:(self.idx+1)*self.batch_size]
+            labels = self.dataset.data[X, self.Y]
+            # labels shape : label_len, batch_size, symbols, columns)
 
             if self.batch_first:
-                return inputs.transpose(1, 0, 2, 3)
+                return inputs.transpose(1, 0, 2, 3), labels.transpose(1, 0, 2, 3)
 
-            return inputs
+            return inputs, labels
         else:
             raise StopIteration
 
 
 # example
 if __name__ == '__main__':
+    # Dataset
+    print('--Dataset test--')
     from datetime import datetime
     import pytz
     if not mt5.initialize():
@@ -164,7 +174,7 @@ if __name__ == '__main__':
     time1 = datetime(2021,1,27,15, tzinfo=timezone)
     time2 = datetime(2021,1,28,15, tzinfo=timezone)
     count = 10
-    timeframe = mt5.TIMEFRAME_M30
+    timeframe = mt5.TIMEFRAME_M20
 
     demo_set = MT5Dataset()
     demo_set.copy(symbol, time1, time2, timeframe=timeframe)
@@ -172,30 +182,13 @@ if __name__ == '__main__':
     print('data[:2, 0] : \n', demo_set.data[:2, 0])
     print('shape of data (count, symbols, columns) : ', demo_set.data.shape)
 
-
-# class a():
-#     def __init__(self):
-#         self.data = torch.randint(0, 10, [11,7,5])
-
-# dataset = a()
-# shape = dataset.data.shape  # (count, symbols, columns)
-# batch_size = 3
-# seq_len = 2
-# idx = 0
-# batch_num = (shape[0]-seq_len+1) // batch_size
-# index = np.stack([np.arange(shape[0]-seq_len+1)] * shape[1])
-# index = np.stack([index+i for i in range(seq_len)])
-# index = index.transpose(0, 2, 1)
-# for i in index[0]:
-#         np.random.default_rng().shuffle(index[0])
-
-# for k in range(1, seq_len):
-#             index[k] = index[k-1]+1
-
-# X = index[:, idx:idx+batch_size]
-
-# # data  : (count, symbols, columns) (11, 7, 5)
-# # index : (seq_len, count-seq_len+1, symbols, ) (2, 10, 7)
-
-# idx = 3
-# dataset.data[X[0, idx:idx + batch_size]]
+    # Dataloader
+    print('--Dataloader test--')
+    seq_len=5
+    batch_size=3
+    label_len=2
+    dataloader = DataLoader(demo_set, seq_len, batch_size, label_len)
+    for i, j in dataloader:
+        print('inputs shape : (seq_len, batch_size, symbols, columns)', i.shape)
+        print('labels shape : (label_len, batch_size, symbols, columns)', j.shape)
+        break
